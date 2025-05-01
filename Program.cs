3bf39059e5b17f148cb9f99248334ccbfa63c1e4
alekskrz <acakrzan@gmail.com>
2025-05-01 14:43:42 +0200

    First commit
    The structure of the project has been made with creating the base for the server

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using SEB.Server;
+
+namespace SEB
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Staring Sports Exercise Battle Server...");
+
+            //Create and start the server
+            HttpServer server = new HttpServer(8080);
+            server.Start();
+        }
+    }
+}
---
+++ Server/HttpServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEB.Server
+{
+    public class HttpServer
+    {
+        private int _port;
+        private TcpListener _listener;
+
+        public HttpServer(int port) 
+        {
+            _port = port;
+        }
+
+        public void Start() 
+        {
+            _listener = new TcpListener(IPAddress.Any, _port);
+            _listener.Start();
+            Console.WriteLine($"Server is listening on port {_port}...");
+
+            while (true) 
+            {
+                TcpClient client = _listener.AcceptTcpClient();
+                Console.WriteLine("Client connected");
+
+                //Handle the client in a new Method (this will be done later)
+                HandleClient(client);
+            }
+        }
+
+        public void HandleClient(TcpClient client) 
+        {
+            //Empty for now
+            Console.WriteLine("Handling client...");
+            client.Close();
+        }
+    }
+}
